d967192891ac3157dbc950d992e61c5c2b93caa4
Margaret Chapman <margaretchapman@wr-130-64-176-133.medford.tufts.edu>
2015-02-14 05:26:46 -0500

    submit button

--- app/src/main/java/idhack2015/com/walimu/InvestigateFragment.java
+++ app/src/main/java/idhack2015/com/walimu/InvestigateFragment.java
@@ -18,8 +18,8 @@ public class InvestigateFragment extends Fragment {
     private final String TAG = "InvestigateFragment";
     private View mLayout;
 
-    public static MonitorFragment newInstance() {
-        MonitorFragment frag = new MonitorFragment();
+    public static InvestigateFragment newInstance() {
+        InvestigateFragment frag = new InvestigateFragment();
         return frag;
     }
 
@@ -29,59 +29,68 @@ public class InvestigateFragment extends Fragment {
                              Bundle savedInstanceState) {
         mLayout = inflater.inflate(R.layout.fragment_investigate, container, false);
 
-        Spinner spinnerTimeFrame = (Spinner) getActivity().findViewById(R.id.spinner);
+        Spinner spinnerTimeFrame = (Spinner) mLayout.findViewById(R.id.spinner);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.timeFrame));
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.timeFrame, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.fragment_investigate);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerTimeFrame.setAdapter(adapter);
 
-        Spinner spinnerCondition = (Spinner) getActivity().findViewById(R.id.spinner2);
+        Spinner spinnerCondition = (Spinner) mLayout.findViewById(R.id.spinner2);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.condition));
         // Specify the layout to use when the list of choices appears
-        adapter2.setDropDownViewResource(R.layout.fragment_investigate);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinnerCondition.setAdapter(adapter2);
+
+        setUpSubmitButton();
 
         return mLayout;
     }
 
-    public void setOptions(View view){
-        Spinner tframe = (Spinner) getActivity().findViewById(R.id.spinner);
-        Spinner cond = (Spinner) getActivity().findViewById(R.id.spinner2);
-        String timeFrame = tframe.getSelectedItem().toString();
-        String condition = cond.getSelectedItem().toString();
-        int dischargeType = 0;
-        int genderType = 0;
-        int hivStatus = 0;
+    public void setUpSubmitButton(){
+        mLayout.findViewById(R.id.buttonInvestigate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner tframe = (Spinner) getActivity().findViewById(R.id.spinner);
+                Spinner cond = (Spinner) getActivity().findViewById(R.id.spinner2);
+                String timeFrame = tframe.getSelectedItem().toString();
+                String condition = cond.getSelectedItem().toString();
+                int dischargeType = 0;
+                int genderType = 0;
+                int hivStatus = 0;
 
-        if (((RadioButton)getActivity().findViewById(R.id.dischargeAll)).isChecked()){
-            dischargeType = 0;
-        }else if (((RadioButton)getActivity().findViewById(R.id.dischargeAlive)).isChecked()){
-            dischargeType = 1;
-        }else {
-            dischargeType = 2;
-        }
-        if (((RadioButton)getActivity().findViewById(R.id.genderAll)).isChecked()){
-            genderType = 0;
-        }else if (((RadioButton)getActivity().findViewById(R.id.genderMale)).isChecked()){
-            genderType = 1;
-        }else { //female
-            dischargeType = 2;
-        }
-        if (((RadioButton)getActivity().findViewById(R.id.hivAll)).isChecked()){
-            hivStatus = 0;
-        }else if (((RadioButton)getActivity().findViewById(R.id.hivPos)).isChecked()){
-            hivStatus = 1;
-        }else { //negative HIV status
-            hivStatus = 2;
-        }
-    makeSearch(timeFrame, condition, dischargeType, genderType, hivStatus);
+                if (((RadioButton)getActivity().findViewById(R.id.dischargeAll)).isChecked()){
+                    dischargeType = 0;
+                }else if (((RadioButton)getActivity().findViewById(R.id.dischargeAlive)).isChecked()){
+                    dischargeType = 1;
+                }else {
+                    dischargeType = 2;
+                }
+                if (((RadioButton)getActivity().findViewById(R.id.genderAll)).isChecked()){
+                    genderType = 0;
+                }else if (((RadioButton)getActivity().findViewById(R.id.genderMale)).isChecked()){
+                    genderType = 1;
+                }else { //female
+                    dischargeType = 2;
+                }
+                if (((RadioButton)getActivity().findViewById(R.id.hivAll)).isChecked()){
+                    hivStatus = 0;
+                }else if (((RadioButton)getActivity().findViewById(R.id.hivPos)).isChecked()){
+                    hivStatus = 1;
+                }else { //negative HIV status
+                    hivStatus = 2;
+                }
+                makeSearch(timeFrame, condition, dischargeType, genderType, hivStatus);
+            }
+        });
     }
+
+
     public void makeSearch(String time, String cond, int discharge, int gender, int hiv){
 //show the results view
 
